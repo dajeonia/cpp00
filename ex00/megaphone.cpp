a52306b21816ba86922c7c7c368217915f0ad226
Daemyeong Jeon <dajeon@c5r3s4.42seoul.kr>
2023-12-04 14:39:03 +0900

    megaphone 1.0
    + str_topper()
    + std::string

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,8 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dajeon <dajeon@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 14:37:09 by dajeon            #+#    #+#             */
+/*   Updated: 2023/12/04 14:38:19 by dajeon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
+
+void	str_toupper(std::string &str);
 
 int	main(int argc, char *argv[])
 {
-	if (argc == 2)
-		std::cout << argv[1] << std::endl;
+	std::string	str;
+
+	if (argc == 1)
+	{
+		str = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		std::cout << str;
+	}
+	for (int i=1; i < argc; ++i)
+	{
+		str = argv[i];
+		str_toupper(str);
+		std::cout << str;
+	}
+	std::cout << std::endl;
 	return (0);
+}
+
+void	str_toupper(std::string &str)
+{
+	for	(int i=0; str[i]; ++i)
+		str[i] = std::toupper(str[i]);
 }
