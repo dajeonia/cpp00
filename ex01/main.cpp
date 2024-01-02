9d15d696d8bab3bda19d0c2119eb4da127cef220
Daemyeong Jeon <dajeon@c5r5s3.42seoul.kr>
2024-01-02 13:20:35 +0900

    main add testcase(copy initializer) and rename the variable

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -7,10 +7,15 @@ int	main(void)
 {
 	PhoneBook book;
 
-	/*
-	Contact dajeon("Daengmyeong", "Jeon", "dajeon", "010-8205-2328", "Hi");
-	book.push(dajeon);
-	*/
+	const std::string index[5] = {"Daengmyeong", "Jeon", "dajeon", "010-8205-2328", "Hi"};
+	std::string index2[5] = {"Daeil", "Jeon", "dajeon", "010-8205-2328", "Hi"};
+
+	Contact test2(index2);
+	Contact test3 = test2;
+
+	book.push(Contact(index));
+	book.push(Contact(test2));
+	book.push(Contact(test3));
 
 	prompt(book);
 	return (0);
@@ -22,14 +27,14 @@ void	prompt(PhoneBook& book)
 	while (true)
 	{
 		std::cout << "\033[0;32m" << "PhoneBook> " << "\033[0;39m";
-		std::string prompt;
+		std::string command;
 
-		getline(std::cin, prompt);
-		if (prompt == "ADD")
+		getline(std::cin, command);
+		if (command == "ADD")
 			book.add();
-		else if (prompt == "SEARCH")
+		else if (command == "SEARCH")
 			book.search();
-		else if (prompt == "EXIT" || std::cin.eof())
+		else if (command == "EXIT" || std::cin.eof())
 		{
 			std::cout << "Goodbye!" << std::endl;
 			break ;
